37ebd4251dae20ca9795806169071153037da785
timeipert <tim.eipert@gmail.com>
2023-06-12 15:33:36 +0200

    Additional Corpus Filter with Callables

--- monodikit/corpus.py
+++ monodikit/corpus.py
@@ -7,14 +7,50 @@ from .genre_specific import ProperTropeComplex
 
 
 class Corpus:
-    def __init__(self, directory, sample=0, filter_options={}):
+    """
+    A collection of chants loaded from a directory.
+
+    Parameters:
+    -----------
+    directory : str
+        The input directory that holds the data.
+
+    sample : int, optional
+        The number of chants that are given as a sample. Defaults to None.
+
+    filter_options : dict or callable, optional
+        If a dict is provided, it does a substring match.
+
+        If a callable object is provided, it calls it for every document and creates the document
+        if True is returned.
+
+    Returns:
+    --------
+    Corpus
+        A Corpus object containing the chants loaded from the directory.
+
+    Examples:
+    ---------
+    Load chants with a specific substring match:
+    ```
+    subcorpus1 = Corpus("../data/*", filter_options={"gattung1": "Sequenz"})
+    ```
+
+    Load chants using a custom filtering function:
+    ```
+    subcorpus2 = Corpus("../data/*", filter_options=lambda chant: chant.gattung1 == "Tropus" or chant.gattung1 == "Sequenz")
+    ```
+    """
+    def __init__(self, directory, sample=0, filter_options=None):
+        if filter_options is None:
+            filter_options = {}
         self.directory = directory
-        self.documents = self.load_corpus(sample)
         self.filter = filter_options
+        self.documents = self.load_corpus(sample)
 
     def load_corpus(self, sample):
         if not sample:
-            return list(filter([create_document(entry, self.filter) for source in glob.glob(self.directory)
+            return list(filter(None, [create_document(entry, filter_options=self.filter) for source in glob.glob(self.directory)
                                 for entry in
                                 [directory for directory in glob.glob(os.path.join(source, "*")) if
                                  os.path.isdir(directory)]
@@ -24,7 +60,7 @@ class Corpus:
                        for entry in
                        [directory for directory in glob.glob(os.path.join(source, "*")) if os.path.isdir(directory)]
                        ]
-            return list(filter(None, [create_document(entry, self.filter)
+            return list(filter(None, [create_document(entry, filter_options=self.filter)
                                       for entry in random.sample(entries, sample)
                                       if check_files_exist(entry)]))
 
@@ -38,11 +74,14 @@ def create_document(entry, filter_options={}):
     """
     meta = Chant.get_meta(entry)
 
-
-    # Now checks only for substring
-    for key, value in filter_options.items():
-        if value not in meta[key]:
+    if callable(filter_options):
+        if not filter_options(meta):
             return None
+    else:
+        # Now checks only for substring
+        for key, value in filter_options.items():
+            if value not in meta.__getattribute__(key):
+                return None
 
     if meta.gattung1 == "Tropus":
         return ProperTropeComplex(entry)
