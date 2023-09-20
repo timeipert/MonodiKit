421903d5f4a39103c020961ef31f0112b071219c
timeipert <tim.eipert@gmail.com>
2023-09-20 13:40:14 +0200

    Docs Changed

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
